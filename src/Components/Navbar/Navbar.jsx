a853d278eac2e2bb53938a809103d4afe3d62b64
Jawwad Junaid <jawwadjunaid17@gmail.com>
2025-02-12 01:09:07 +0500

    initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
-import "./Navbar.css"; // Import CSS file
+import "./Navbar.css";
+import logo from '../../assets/images/images/logo.png'
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="navbar">
-      <img src="/logo.png" alt="Logo" className="logo" />
+      {/* Logo */}
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
 
       {/* Hamburger Menu for Mobile */}
       <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
         ☰
       </div>
 
+      {/* Navigation Links */}
       <div className={`nav-links ${isOpen ? "open" : ""}`}>
         <a href="#">Home</a>
         <a href="#">About Us</a>
+        <a href="#">Service</a>
         <a href="#">Contact</a>
-        <a href="#">Hiring</a>
-        <button className="appointment-btn">Appointment</button>
       </div>
+
+      {/* Appointment Button */}
+      <button className="appointment-btn">Book Appointment</button>
     </nav>
   );
 };
