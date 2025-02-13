c66d3cf1ec4d8cdb3861fe632715f192bb83ca35
Jawwad Junaid <jawwadjunaid17@gmail.com>
2025-02-14 04:22:25 +0500

    updated All mess

--- src/Components/Navbar1/Navbar1.jsx
+++ src/Components/Navbar1/Navbar1.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar1.css";
-import NavLogo from '../../assets/images/images/Logo.png';
+import NavLogo from '../../assets/images/images/logo.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
