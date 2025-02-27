58ac4b73c570357f28c1bee127439f0ed31de6a7
Jawwad Junaid <jawwadjunaid17@gmail.com>
2025-02-27 23:58:22 +0500

    updated map

--- src/Components/MapComponent/MapComponent.jsx
+++ src/Components/MapComponent/MapComponent.jsx
@@ -6,21 +6,38 @@ const containerStyle = {
 };
 
 const center = {
-  lat: 29.739456895552287, // Change to your desired latitude
-  lng:  -95.37666955916286, // Change to your desired longitude
+  lat: 29.739456895552287, 
+  lng: -95.37666955916286, 
 };
 
 const MapComponent = () => {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyACAAZXFGYFY_3Gdkm3NGX1FkG_7M7FrEE">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-        {/* Marker at center */}
-        <Marker position={center}
-        icon={{
-            url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png", // Red marker icon URL
-          }} />
-      </GoogleMap>
-    </LoadScript>
+    <div>
+      {/* Google Maps API-based Interactive Map */}
+      {/* <LoadScript googleMapsApiKey="YOUR_GOOGLE_MAPS_API_KEY">
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
+          <Marker
+            position={center}
+            icon={{
+              url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png", 
+            }}
+          />
+        </GoogleMap>
+      </LoadScript> */}
+
+      {/* Embedded Google Maps Iframe */}
+      <div style={{ marginTop: "20px", textAlign: "center" }}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3464.323832055782!2d-95.3793164244268!3d29.739339775078673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640bf6dbe20420d%3A0x9de6bc0b6f8642c4!2s3402%20San%20Jacinto%20St%2C%20Houston%2C%20TX%2077004%2C%20USA!5e0!3m2!1sen!2s!4v1740682356662!5m2!1sen!2s"
+          width="600"
+          height="450"
+          style={{ border: "0", width: "100%", maxWidth: "" }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
   );
 };
 
